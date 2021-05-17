13ce6007fdc77e9eb71f8e9cacbb67338e1f323d
Peter Öttl <peter.oettl@allaboutapps.at>
2021-05-17 17:55:39 +0200

    console ReadString

--- 03_State/main.go
+++ 03_State/main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 	"swp/state/gamestore"
 )
 
 func main() {
 	gamestore := gamestore.NewGame()
-	gamestore.BuyGame()
-	gamestore.BuyGame()
-	gamestore.PlayGame()
-	gamestore.InstallGame()
-	gamestore.PlayGame()
-	gamestore.UninstallGame()
 
-	// err = vendingMachine.insertMoney(10)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Gamestore")
+	fmt.Println("---------------------")
+	fmt.Println("Available Commands:")
+	fmt.Println()
+	fmt.Println("buy")
+	fmt.Println("install")
+	fmt.Println("play")
+	fmt.Println("stop")
+	fmt.Println("uninstall")
+	fmt.Println("exit")
+	fmt.Println()
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("buy", text) == 0 {
+			gamestore.BuyGame()
+		}
+		if strings.Compare("install", text) == 0 {
+			gamestore.InstallGame()
+		}
+		if strings.Compare("play", text) == 0 {
+			gamestore.PlayGame()
+		}
+		if strings.Compare("stop", text) == 0 {
+			gamestore.StopGame()
+		}
+		if strings.Compare("uninstall", text) == 0 {
+			gamestore.UninstallGame()
+		}
+		if strings.Compare("exit", text) == 0 {
+			break
+		}
+
+	}
 
 }
